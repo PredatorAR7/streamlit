ae8e0561f1baf1f038dbd15748c7ce64ff260cc5
Aditya Raj Raizada <161312262+PredatorAR7@users.noreply.github.com>
2024-06-28 19:51:22 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ from vipas import model
 from vipas.exceptions import UnauthorizedException, NotFoundException, RateLimitExceededException
 import base64
 import io
+import boto3
+from boto3 import session
 
 # Set the title and description
 st.title("🔍 YOLOv8n Object Detection")
