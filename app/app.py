ae6f581855573eb0da93acea698bff5ac4da284c
Aditya Raj Raizada <161312262+PredatorAR7@users.noreply.github.com>
2024-09-09 15:48:24 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -149,7 +149,7 @@ if uploaded_file is not None:
         try:
             # Simulate high memory consumption to trigger OOMKilled
             st.warning("Simulating memory overload. This may cause the container to crash.")
-            large_data = np.zeros((10240, 10240, 10240, 10240))  # Intentional high memory allocation
+            large_data = np.zeros((10240, 1024, 1024, 1024))  # Intentional high memory allocation
 
             api_response = vps_model_client.predict(model_id=model_id, input_data=img_str)
             output_base64 = postprocess(api_response, image.copy())
